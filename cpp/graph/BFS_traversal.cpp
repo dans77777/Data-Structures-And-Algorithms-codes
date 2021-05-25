75f39c87573e91494be9f5450d679a3cd68d2a3c
dans77777 <61909995+dans77777@users.noreply.github.com>
2021-05-25 09:24:19 +0530

    BFS traversal

---
+++ cpp/graph/BFS_traversal.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+void bfs(vector<vector<int>> v, int sv, vector<bool>&visited)
+{
+    int n=v.size();
+    queue<int>q;
+    q.push(sv);
+    visited[sv]=true;
+    while(!q.empty())
+    {
+        int cv=q.front();
+        cout<<cv<<endl;
+        q.pop();
+        for(int i=0;i<n;i++)
+        {
+            if(v[cv][i]==1 && visited[i]==false)
+            {
+                q.push(i);
+                visited[i]=true;
+            }
+        }
+
+
+    }
+}
+int main()
+{
+    int n,e;
+    cout<<"number of vertex"<<endl;
+    cin>>n;
+    vector<vector<int>>v(n,vector<int>(n,0));
+    cin>>e;
+     vector<bool>visited(n,false);
+    for(int i=1;i<=e;i++)
+    {
+        int v1,v2;
+        cout<<"enter vertex 1 and  vertex 2"<<endl;
+        cin>>v1>>v2;
+        v[v1][v2]=1;
+         v[v2][v1]=1;
+
+    }
+      bfs(v,0,visited);
+return 0;
+
+}
